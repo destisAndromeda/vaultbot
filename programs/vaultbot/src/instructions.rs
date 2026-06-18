a9e60a6a07ddf36d40edf5150e6a954492f56471
None <destis_Andromeda@proton.me>
2026-06-18 15:32:40 +0200

    delete smart_wallet feature

--- programs/vaultbot/src/instructions.rs
+++ programs/vaultbot/src/instructions.rs
@@ -1,9 +1,7 @@
-// pub mod initialize_smart_wallet;
 pub mod initialize_vault;
 pub mod deposit;
 pub mod withdraw;
 
-// pub use initialize_smart_wallet::*;
 pub use initialize_vault::*;
 pub use deposit::*;
 pub use withdraw::*;
--- programs/vaultbot/src/state.rs
+++ programs/vaultbot/src/state.rs
@@ -1,5 +1,3 @@
-// pub mod smart_wallet;
 pub mod vault;
 
-// pub use smart_wallet::*;
 pub use vault::*;
